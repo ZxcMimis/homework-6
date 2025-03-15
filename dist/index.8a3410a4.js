25c2af509bcd79734b2ec6e21439483f71c5964a
Руслан Івасюк <126970988+ZxcMimis@users.noreply.github.com>
2025-03-15 13:49:22 +0200

    title

---
+++ dist/index.8a3410a4.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".email"),t=document.querySelector(".name"),u=document.querySelector(".text");document.querySelector("button").addEventListener("click",l=>{l.preventDefault(),""===e.value||""===t.value||""===u.value?alert("Type something!"):alert("Sucsesfull!")});
+//# sourceMappingURL=index.8a3410a4.js.map
